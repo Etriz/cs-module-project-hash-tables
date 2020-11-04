909236a5747e495496037379923893302b506c3f
ryanp <ryanpanimation@gmail.com>
2020-11-04 00:46:15 -0600

    no dups test OK

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,6 +1,18 @@
 def no_dups(s):
     # Your code here
+    word_dict = dict()
+    word_list = s.split()
 
+    for w in word_list:
+        if w not in word_dict:
+            word_dict[w] = True
+
+    word_dict = " ".join(word_dict)
+
+    return word_dict
+
+    # this doesn't work because sets are unordered
+    # return " ".join(set(s.split()))
 
 
 if __name__ == "__main__":
